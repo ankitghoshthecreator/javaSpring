bbd98fd3632de49f204f75607b869eb6c552c11b
ankitghoshthecreator <baruipara01@gmail.com>
2026-06-19 10:44:51 +0530

    first initial commit just the structure

---
+++ javaSpringgapp/src/main/java/com/Ankit/javaSpringgapp/JavaSpringgappApplication.java
@@ -0,0 +1,13 @@
+package com.Ankit.javaSpringgapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringgappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringgappApplication.class, args);
+	}
+
+}
---
+++ javaSpringgapp/src/test/java/com/Ankit/javaSpringgapp/JavaSpringgappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ankit.javaSpringgapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringgappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
